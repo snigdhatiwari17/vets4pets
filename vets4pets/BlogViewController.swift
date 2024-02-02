af7c4a2f543d58fbb50405c4b53ad49c2c2c2f11
Snigdha Tiwari <snigdha.17.tiwari@gmail.com>
2024-02-02 19:56:39 +0530

    news functionality

---
+++ vets4pets/BlogViewController.swift
@@ -0,0 +1,131 @@
+//
+//  BlogViewController.swift
+//  vets4pets
+//
+//  Created by Snigdha Tiwari  on 02/02/24.
+//
+
+import SafariServices
+import UIKit
+
+// TableView
+// Custom Cell
+// API Caller
+// Open the News Story
+// Search for News Stories
+
+class BlogViewController: UIViewController {
+    
+    //Create a tableView
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        return table
+    }()
+    
+    private let searchVC = UISearchController(searchResultsController: nil)
+    
+    private var viewModels = [NewsTableViewCellViewModel]()
+    private var articles = [Article]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "News"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchTopStories()
+        createSearchBar()
+    }
+    private func fetchTopStories () {
+        APICaller.shared.getTopStories { [weak self] result in
+            switch result{
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(
+                        title: $0.title,
+                        subtitle: $0.description ?? "No Description",
+                        imageURL: URL(string: $0.urlToImage ?? "https://picsum.photos/200/300")
+                    )
+                })
+                // Once ViewModels are ready, table view should refresh
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+        //MARK: - ViewDidLayoutSubViews
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func createSearchBar(){
+        navigationItem.searchController = searchVC
+        searchVC.searchBar.delegate = self
+    }
+
+}
+// Extend View Controller for TableView
+extension BlogViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NewsTableViewCell.identifier,
+            for: indexPath
+        ) as? NewsTableViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let article = articles[indexPath.row]
+        guard let url = URL(string: article.url ?? "") else {
+            return
+        }
+        let vc = SFSafariViewController(url: url)
+        present(vc,animated: true)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    //Search
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.isEmpty else {
+            return
+        }
+        APICaller.shared.search(with: text) { [weak self] result in
+            switch result{
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(
+                        title: $0.title,
+                        subtitle: $0.description ?? "No Description",
+                        imageURL: URL(string: $0.urlToImage ?? "")
+                    )
+                    
+                })
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                    self?.searchVC.dismiss(animated: true, completion: nil)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+}
+
---
+++ vets4pets/SecretKey.swift
@@ -0,0 +1,12 @@
+//
+//  SecretKey.swift
+//  NewsApp
+//
+//  Created by Sree Sai Raghava Dandu on 13/05/21.
+//
+
+import Foundation
+
+struct Secret {
+    static let secretKey: String = "799d63f9e9874076b806c1ed484e04c0"
+}
